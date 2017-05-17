030b57a36756fdc60a49ec2ae8b0c21665c62f7d
Dan Parry <parryd424@gmail.com>
2017-05-17 12:32:14 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
-});
+  submitForm()
+})
 
 // define functions here
+function submitForm(){
+  $('form').on('submit',function(e){
+    var itemToAdd = $('#item').val()
+    $('ol').append(`<li>${itemToAdd}</li>`)
+    e.preventDefault()
+  })
+}
